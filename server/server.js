b5c216e1756e6b6a2c1fedc829863e5a5418d3d3
Eldor Togaymurotov <eldorcodes@gmail.com>
2018-05-27 13:23:47 -0700

    new changes made

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const app = express();
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('Connected to the Client');
+    socket.on('disconnect', (socket) => {
+        console.log('Disconnected from the Client');
+    });
+});
+
+server.listen(port, () => {
     console.log(`ChatAPP is running on port ${port}`);
 });
