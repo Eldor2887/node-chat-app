845a833664bff64eeecfd36ae3e44338a2e44d42
Eldor Togaymurotov <eldorcodes@gmail.com>
2018-05-30 13:15:38 -0700

    added form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,5 +9,17 @@
 
  socket.on('newMessage', function(message) {
      console.log('newMessage', message);
-     document.getElementById('message').innerHTML = message.from + ': ' + message.text;
+     var li = jQuery('<li></li>');
+     li.text(`${message.from}: ${message.text}`);
+     jQuery('#messages').append(li);
+ });
+
+ jQuery('#message-form').on('submit', function(e){
+     e.preventDefault();
+     socket.emit('createMessage', {
+         from: 'User',
+         text: jQuery('[name=message]').val()
+     }, function(){
+
+     })
  });
