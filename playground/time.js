871ab650b07117bb4c2d3623fccc54efa926cea1
Eldor Togaymurotov <eldorcodes@gmail.com>
2018-06-06 00:43:20 -0700

    moment added

---
+++ playground/time.js
@@ -0,0 +1,6 @@
+const moment = require('moment');
+
+var date = moment(1234);
+
+console.log(date.format('MMM Do YYYY'));
+console.log(date.format('hh : mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
  var socket = io();
+ 
  socket.on('connect', function () {
      console.log('Connected to the Server.');
  });
@@ -8,16 +9,18 @@
  });
 
  socket.on('newMessage', function(message) {
+     var formattedTime = moment(message.createdAt).format('h:mm a');
      console.log('newMessage', message);
      var li = jQuery('<li></li>');
-     li.text(`${message.from}: ${message.text}`);
+     li.text(`${message.from} ${formattedTime}: ${message.text}`);
      jQuery('#messages').append(li);
  });
 
  socket.on('newLocationMessage', function(message){
+     var formattedTime = moment(message.createdAt).format('h:mm a');
      var li = jQuery('<li></li>');
      var a = jQuery('<a target="_blank">My Current Location</a>');
-     li.text(`${message.from}: `);
+     li.text(`${message.from} ${formattedTime}: `);
      a.attr('href', message.url);
      li.append(a);
      jQuery('#messages').append(li);
